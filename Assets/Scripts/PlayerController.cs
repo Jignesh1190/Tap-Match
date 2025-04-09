58fe7645b93db75b3cbf95166abc257055cfe5f8
Jignesh1190 <104572020+Jignesh1190@users.noreply.github.com>
2025-04-09 09:50:37 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotationSpeed = 1f; // Sensitivity of rotation
+    public float smoothTime = 0.5f;    // Smoothing time
+
+    private Vector3 lastTouchPosition;
+    private Vector3 touchDelta;
+    private Vector3 currentVelocity;
+
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
+
+    void Start()
+    {
+        currentRotation = transform.localEulerAngles;
+        targetRotation = currentRotation;
+    }
+
+    void Update()
+    {
+        HandleTouchInput();
+        SmoothRotate();
+    }
+
+    void HandleTouchInput()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                touchDelta = touch.deltaPosition;
+
+                targetRotation.y += touchDelta.x * rotationSpeed;
+                targetRotation.x -= touchDelta.y * rotationSpeed;
+            }
+            else
+            {
+                // No swipe movement — stop changing rotation
+                touchDelta = Vector2.zero;
+            }
+        }
+    }
+      void SmoothRotate()
+    {
+        currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation, ref currentVelocity, smoothTime);
+        transform.localEulerAngles = currentRotation;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Tap---Match
+ Basic Mobile game where you need to match color.
